6d8a8b9deaf661f94681391d4ad75407855704b0
lauandsalih <lauandsalih@hotmail.fi>
2026-04-20 03:26:48 +0300

    fix: set session to persist 30 days

--- frontend/src/supabase.ts
+++ frontend/src/supabase.ts
@@ -3,4 +3,12 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+  auth: {
+    persistSession: true,
+    // 30 days in seconds
+    persistSessionDuration: 30 * 24 * 60 * 60,
+    autoRefreshToken: true,
+    detectSessionInUrl: true
+  }
+})
